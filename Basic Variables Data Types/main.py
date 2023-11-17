f68528dcc4376688daa4d27f893c2a45f0f9d4c8
Abqary <fathin.abqary@student.alhazen.academy>
2023-11-17 09:03:53 +0700

    tambah script basic, operator, condition

---
+++ Basic Variables Data Types/main.py
@@ -0,0 +1,16 @@
+print("Binary Operator")
+
+number1 = int(input("Masukkan Angka Pertama: "))
+number2 = int(input("Masukkan Angka Kdeua: "))
+
+and_operator = number1 & number2
+or_opetaror = number1 | number2
+xor_operator = number1 ^ number2
+not_operator = number1
+not_operator = number2
+
+print(f"{number1} & {number2} = {and_operator}")
+print(f"{number1} | {number2} = {or_opetaror}")
+print(f"{number1} ^ {number2} = {xor_operator}")
+print(f"~{number1} = {not_operator}")
+print(f"~{number2} = {not_operator}")
